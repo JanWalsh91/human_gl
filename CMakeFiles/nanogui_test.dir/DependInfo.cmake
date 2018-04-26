3ef1ac2e6a573ed63c7b75e5e9dfc036b4328805
Theo GROS <tgros@e2r10p1.42.fr>
2018-04-26 14:48:46 +0200

    gui first test

---
+++ CMakeFiles/nanogui_test.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tgros/Projects/human_gl/example3.cpp" "/Users/tgros/Projects/human_gl/CMakeFiles/nanogui_test.dir/example3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "libs/nanogui/include"
+  "libs/nanogui/ext/glfw/include"
+  "libs/nanogui/ext/nanovg/src"
+  "libs/nanogui/ext/eigen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/tgros/Projects/human_gl/libs/nanogui/CMakeFiles/nanogui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
